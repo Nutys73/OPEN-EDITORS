2bcdc6f333c6e0ef8767ebf9407e02d3c6980fbd
unknown <natalie.stefkova@infis.cz>
2025-04-01 08:40:27 +0200

    ukolprodva

---
+++ ukolprodva.py
@@ -0,0 +1,10 @@
+#Štefková
+jidlo1=input("Zadejte jídlo: ")
+kalorie1=int(input("Zadejte počet kalorií: "))
+jidlo2=input("Zadejte jídlo: ")
+kalorie2=int(input("Zadejte počet kalorií: "))
+jidlo3=input("Zadejte jídlo: ")
+kalorie3=int(input("Zadejte počet kalorií: "))
+celkoveKalorie=kalorie1+kalorie2+kalorie3
+print(celkoveKalorie,"kcal")
+
